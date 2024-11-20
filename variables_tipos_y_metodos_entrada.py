e8d012ef8125e94729936a94b0bccdafd53a0f9c
Carlos S. Castillo <castillodepaz@gmail.com>
2024-11-20 12:39:12 -0600

    Mover a otra carpeta

---
+++ variables_tipos_y_metodos_entrada.py
@@ -0,0 +1,15 @@
+# #Variables
+# variable = 7
+# nombre = "Carlos Castillo"
+
+# #metodos de salida
+# print("Hola mundo")
+
+# #metodos de entradas
+# numero1 = 9
+# numero2 = 8
+
+# resultado = numero1 + numero2
+
+# print(resultado)
+
