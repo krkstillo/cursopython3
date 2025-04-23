2aca3e6ef996f52f9bf030c681e1a78cf5092f48
Carlos S. Castillo <castillodepaz@gmail.com>
2025-04-23 17:19:52 -0600

    cambio de carpeta

--- fundamentos/variables_tipos_y_metodos_entrada.py
+++ fundamentos/variables_tipos_y_metodos_entrada.py
@@ -1,9 +1,9 @@
 #Variables
-variable = 7
-nombre = "Carlos Castillo"
+#variable = 7
+#nombre = "Carlos Castillo"
 
 #metodos de salida
-print("Hola mundo")
+#print("Hola mundo")
 
 #metodos de entradas
 #numero1 = 9
@@ -11,4 +11,12 @@ print("Hola mundo")
 
 #resultado = numero1 + numero2
 
-print(resultado)
+#print(resultado)
+
+# numero1 = float(input('Ingesa un número  '))
+# numero2 = float(input('Ingresa un número '))
+
+# resultado = numero1 + numero2
+
+# print(resultado)
+
