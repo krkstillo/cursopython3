c5ce5641c2c6a5e6831fdd24de235ccd0e17e6f0
Carlos S. Castillo <castillodepaz@gmail.com>
2025-04-28 16:18:06 -0600

    actualizacion de funciones

---
+++ funciones/introduccion.py
@@ -0,0 +1,22 @@
+#def sumar():
+#    resultado = 7 + 3
+#    print(resultado)
+
+
+#sumar()
+
+#Con parametros
+#def sumar(a, b):
+#    resultado = a + b
+#    return resultado
+
+#print(sumar(2,3))
+
+#Con parametros por defecto
+#def sumar(a = 5, b = 2):
+#    resultado = a + b
+#    return resultado
+
+#print(sumar())
+#print(sumar(4,6))
+
---
+++ funciones/lambda.py
@@ -0,0 +1,6 @@
+#def sumar(a, b):
+#    return a + b
+
+sumar = lambda a, b:(a + b)
+
+print(sumar(7,8))
