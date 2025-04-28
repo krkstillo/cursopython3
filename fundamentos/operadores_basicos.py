e59c04bf03cc51c2aa3e72409af79836022f32b4
Carlos S. Castillo <castillodepaz@gmail.com>
2025-04-28 10:25:07 -0600

    Agrega archivos

---
+++ fundamentos/operadores_basicos.py
@@ -0,0 +1,27 @@
+"""
+OPERADORES ARITMETICOS
+"""
+numero1 = float(input('Ingesa un número  '))
+numero2 = float(input('Ingresa un número '))
+
+# print(f'Suma {numero1 + numero2}')
+# print(f'Resta {numero1 - numero2}')
+# print(f'Multiplicación {numero1 * numero2}')
+# print(f'División {numero1 / numero2}')
+# print("Módulo ", (numero1 % numero2))
+# print("Potencia ", (numero1 ** numero2))
+# print("División exacta ", (numero1 // numero2))
+
+"""
+OPERADORES RELACIONALES
+"""
+print(" >  ", numero1 > numero2)
+print(" <  ", numero1 < numero2)
+print(" >= ", numero1 >= numero2)
+print(" <= ", numero1 <= numero2)
+print(" == ", numero1 == numero2)
+print(" != ", numero1 != numero2)
+
+"""
+OPERADORES DE ASIGNACIÓN
+"""
